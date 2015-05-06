c67c50e8f95de10d6a7c2e4c729efa85dec77d9a
ahars <antoine.hars@gmail.com>
2015-05-06 16:13:51 +0200

    separated compartiments for local processing and hadoop processing

--- local_process/run_docker_flume.sh
+++ local_process/run_docker_flume.sh
@@ -16,7 +16,7 @@ $(boot2docker shellinit)
 docker build --rm -t ahars/hdp-flume-sandbox .
 docker run -t -i --rm \
 	--name flume \
-	-v /Users/ahars/Github/hdp-flume-sandbox/:/opt/hdp-flume-sandbox \
+	-v /Users/ahars/Github/hdp-flume-sandbox/local_process/:/opt/hdp-flume-sandbox/ \
 	-e CONF_FILE=$1 \
 	ahars/hdp-flume-sandbox
 
