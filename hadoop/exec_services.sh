acc15b3ae2aef61b97acbd69636a72eb71a9272e
ahars <antoine.hars@gmail.com>
2015-05-18 14:20:07 +0200

    config of HDFS for flume

--- hadoop/exec_services.sh
+++ hadoop/exec_services.sh
@@ -10,9 +10,20 @@ fi
 /usr/hdp/current/hadoop-hdfs-namenode/../hadoop/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start namenode
 /usr/hdp/current/hadoop-hdfs-datanode/../hadoop/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start datanode
 
+hadoop fs -rm -r /ACCEPTED 
+hadoop fs -rm -r /REJECTED
 hadoop fs -mkdir /ACCEPTED
 hadoop fs -mkdir /REJECTED
 
-# Launch Flume agent
-flume-ng agent -n agent_$1 -c conf -f ../conf/flume_$1.conf -Dflume.root.logger=INFO,console
+mv -v data/input/example.log.COMPLETED data/input/example.log
 
+# Launch Flume agent
+flume-ng agent -n agent_$1 -c conf -f conf/flume_$1.conf -Dflume.root.logger=INFO,console
+
+echo
+echo ACCEPTED :
+hadoop fs -ls /ACCEPTED
+echo
+echo REJECTED :
+hadoop fs -ls /REJECTED
+echo
