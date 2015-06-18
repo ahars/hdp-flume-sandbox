d7655bec3fae9abff0c69f2f8771a19fc888cf40
ahars <antoine.hars@gmail.com>
2015-06-18 18:36:16 +0200

    refactoring sand5

---
+++ hadoop/sand5/run_sand5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+hadoop fs -rm -r /user/root/data/* 
+
+mv -v data/input/example.log.COMPLETED data/input/example.log
+
+cd sand5/flume-serializer-sand5 && mvn package
+mv -vf target/flume-serializer-sand5-1.0-SNAPSHOT.jar /usr/hdp/2.2.0.0-2041/flume/lib/
+cd ../..
+
+# Launch Flume agent
+flume-ng agent -n sand5 -c conf -f sand5/flume_5.conf -Dflume.root.logger=INFO,console
+
+echo
+echo ACCEPTED :
+hadoop fs -ls -R /user/root/data/ACCEPTED
+echo
+
